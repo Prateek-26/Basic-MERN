909a6afa92047d29fdec5b594ba9becfb3291f68
Prateek Sharma <sharmaprateek.dev@gmail.com>
2023-05-04 01:36:27 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [form, setForm] = useState({});
+  const [users, setUsers] = useState([]);
+
+  const handleForm = (e) => {
+    console.log(e.target.value);
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post("http://localhost:8080/demo", {
+        form,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  // Now if we log here, we'll get CORS error, since servers are different and its not allowed that one directly tries to access another. This can be allowed / resolved at the servers.
+ 
+  const getUsers = async () => {
+    await axios
+      .get("http://localhost:8080/demo")
+      .then((res) => {
+        // console.log(res);
+        const data = res.data;
+        console.log(data);
+        setUsers(data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(()=>{
+getUsers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        {/* <h1>{ JSON.stringify(form) }</h1>  */}
+        <input
+          type="text"
+          onChange={handleForm}
+          name="uname"
+          id=""
+          placeholder="username"
+        />
+        <input
+          type="password"
+          onChange={handleForm}
+          name="pword"
+          id=""
+          placeholder="password"
+        />
+        {/* <input type="submit" value="" placeholder=""/> */}
+        <button type="submit">Enter</button>
+      </form>
+      <div>
+        <ul>
+          {users.map(user => <li key={user._id}>{user.username}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
